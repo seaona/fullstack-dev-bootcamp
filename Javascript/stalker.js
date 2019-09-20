e4ef836113475264983831c701cb9aee1cefab01
Seaona <mariona@gmx.es>
2019-09-20 11:07:24 +0200

    Stalker Exercise

---
+++ Javascript/stalker.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What's your first name?");
+var userLastName = prompt("What's your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("User first name is " + userFirstName + " and last name is " + userLastName);
+console.log("User age is " + userAge);
