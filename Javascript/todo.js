fb4e6fcc0e1891d2f4e613d92d08324264b5c72a
Seaona <mariona@gmx.es>
2019-09-27 09:09:48 +0200

    Todos with arrays and for Each

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -2,13 +2,40 @@ var todos=[];
 var input = prompt("What would you like to do?");
 
 while(input!=="quit"){
+	//handle input
     if(input==="new"){
-	    var todo=prompt("Add a Todo");
-	    todos.push(todo);
+    	addTodo();
 	}
 	else if(input==="list"){
-	    console.log(todos);
+		listTodos();  
 	}
-	input = prompt("What would you like to do?");
+	else if(input==="delete"){
+		deleteTodo();
       }
+	//ask again for new input
+	input = prompt("What would you like to do?");
+}
 console.log("you quit");
+
+
+function listTodos(){
+	console.log("********");
+	todos.forEach(function(todo, index){
+	console.log(index+":"+todo);
+	});
+	console.log("********");
+}
+
+function addTodo(){
+	var todo=prompt("Add a Todo");
+	todos.push(todo);
+	console.log("you added");
+}
+
+function deleteTodo(){
+	//ask for index of todo to be deleted
+	var todoIndex=prompt("Specify an index");
+	//delete todo (which one, and from there, how many)
+	todos.splice(todoIndex,1);
+	console.log("you deleted item "+todoIndex);
+}
