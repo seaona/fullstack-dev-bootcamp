4137a5aaf11755a5f1524c0d91c9ba2d86d64a74
seaona <mariona@gmx.es>
2019-11-04 09:29:46 +0100

    NPM exercises

---
+++ NPM/DemoApp/app.js
@@ -0,0 +1,5 @@
+var cat=require("cat-me"); //include name of the package cat-me
+var joke=require("knock-knock-jokes"); //include name of the package knock-knock-jokes
+
+console.log(cat());
+console.log(joke());
