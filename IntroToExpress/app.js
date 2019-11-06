aaee2b9ec1f5a8a2be7db91d4ebfa68471bc0171
seaona <mariona@gmx.es>
2019-11-06 08:39:54 +0100

    adding express and running a server

---
+++ IntroToExpress/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+//everywhere where we use express, we will write "app. something"
+var app= express();
+
+//3 different ROUTES
+
+// "/" => "Hi there!!"
+app.get("/", function(req,res){
+    res.send("hi there");
+});
+// "/bye" => "Goodbye"
+app.get("/bye", function(req,res){
+    res.send("goodbye");
+});
+// "/dog" => "MEOW"
+app.get("/dog", function(req,res){
+    console.log("someone made a request")
+    res.send("meow");
+});
+
+//tell Express to listen for requests (start server)
+app.listen(3000, function(){
+    console.log("server has started");
+});
