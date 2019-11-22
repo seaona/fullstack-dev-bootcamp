8893f938da924a855ae9272972fd771ceca8094d
seaona <mariona@gmx.es>
2019-11-22 08:23:54 +0100

    Working with EJS

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinginlovewith/:thing", function(req,res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "post 1", author: "susy"},
+        {title: "my adorable pet bunny", author: "charlie"},
+        {title: "can you believe this pomsky", author: "mar"},
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
 
 app.listen(3000, function(){
     console.log("server is listening");
