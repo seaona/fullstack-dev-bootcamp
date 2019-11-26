cd900fc59fbba60adeb5c31cef7004ff4d6cd970
seaona <mariona@gmx.es>
2019-11-26 13:17:49 +0100

    API response

--- APIs/first-request.js
+++ APIs/first-request.js
@@ -1,14 +1,10 @@
 var request=require("request");
 
-request("https://www.googlekvfkdsgjdñsgj.com", function(error, response, body){
-    if(error){
-        console.log("Something went wrong");
-        console.log(error);
-    }   
-    else{
-        if(response.statusCode ==200){
-            console.log(body);
-        }
-    } 
+console.log("Weather description is...")
 
+request("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22", function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["weather"]["0"]["description"]);
+    }   
 });
