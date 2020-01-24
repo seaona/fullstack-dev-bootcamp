4144d159a2893e5170ea3dace853d2be1dff423f
seaona <mariona@gmx.es>
2020-01-24 09:06:00 +0100

    More styling

---
+++ YelpCamp/v5/app.js
@@ -0,0 +1,123 @@
+var express = require("express");
+var app = express();
+var bodyParser=require("body-parser");
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public")); //__dirname is the directory where this script lives in
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser:true, useUnifiedTopology:true});
+seedDB(); //we call the function that we export within seeds.js
+
+
+
+//ROUTES
+app.get("/", function(req,res){
+    res.render("landing");
+});
+
+//INDEX Show all campgrounds
+app.get("/campgrounds", function(req,res){
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds: allCampgrounds}); 
+            //
+            //to campgrounds.ejs we will pass the data from db
+        }
+    }); 
+});
+
+//CREATE Add new campground db
+app.post("/campgrounds", function(req,res){ //restful convention. This route and the above have the same name, but they are different routes, because the above it's a get, and this is a post
+    //get data from form and add to campgrounds array defined on top
+    var name= req.body.name;
+    var image= req.body.image;
+    var desc = req.body.description;
+    var newCampground= {name: name, image: image, description: desc};
+    
+    //create a new campground and save it to the DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds"); //when we do a redirect, the default is redirect as a GET request
+        }
+    });
+});
+
+//NEW show form to create a new campground
+app.get("/campgrounds/new", function(req,res){ //this is another Restful convention
+    res.render("campgrounds/new");
+    
+});
+
+
+//SHOW show more info about a specific campground
+app.get("/campgrounds/:id", function(req, res){ //this is for any url following campgrounds/anything. That's why we need to put it after NEw
+    //find the campground with the provided id
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground);
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+
+//=====================================
+//COMMENTS ROUTES
+//=====================================
+
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    }); 
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            //create a new comment and save it to the DB
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+
+                    //redirect to campground show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });          
+        }
+    });
+});
+
+
+//STARTING THE SERVER
+app.listen(3000, function(){
+    console.log("YelpCamp Server has started");
+});
+
+//ENDING SERVER
---
+++ YelpCamp/v5/models/campground.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId, //it's an array of comments id. We are not embedding the comments here
+            ref: "Comment" //the name of the model
+        }
+    ]
+});
+
+//when we require campground modul, we will get this:
+module.exports = mongoose.model("Campground", campgroundSchema);
